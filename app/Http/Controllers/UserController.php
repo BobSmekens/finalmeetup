b3d6148a6b6cc788380ee4ab29898249f9832f3d
Bob Smekens <bobsmekens90@gmail.com>
2019-03-13 14:49:31 +0100

    delete account added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -106,6 +106,10 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        
+        $user->delete();
+
+        return redirect('/');
     }
 }
