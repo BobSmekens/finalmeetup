a8de46fa895ce826561080bb09b76f3eb66c0b4e
KCTHolman <koenholman@gmail.com>
2019-03-14 14:48:53 +0100

    activity index masic bootstrap

--- resources/views/activities/index.blade.php
+++ resources/views/activities/index.blade.php
@@ -1,15 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>Activities</h3>
-    
-    <p>All activities, sort by skills or activity</p>
-
-    @foreach($activities as $activity)
-        <div>
-            <a href="/activities/{{ $activity->id }}">{{ $activity->activity }}</a>   
+<div class="container m-0 p-0 justify-content-center">
+    <div class="my-row row m-0 p-0 bg-danger align-content-center justify-content-center">
+        <div class="col">
+            <h3>Activities</h3>
         </div>
-    @endforeach
-    <br>
-    <a href="/activities/create">Add activity</a>
+        <div class="col">
+            <p>All activities, sort by skills or activity</p>
+        </div>
+    </div>
+    <div class="my-row row m-4 p-4 ">
+        @foreach($activities as $activity)
+        <div class="h-25 col-12 m- p-0 d-flex justify-content-start">
+        <div class="col-3">1/<span>{{$activity->max_persons}}</div> <div class="col-7"><a href="/activities/{{ $activity->id }}">
+        {{ $activity->activity }} </a></div>
+        <div class="col-4"> door: <span class=>{{$activity->posted_by}}</div>
+        </div>
+        @endforeach
+    </div>
+    <a class="btn btn-danger m4" href="/activities/create">Add activity</a>
+</div>
 @endsection
