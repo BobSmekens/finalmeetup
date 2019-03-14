779a79ba689a1f90bcead21d87c9743de8167b18
KCTHolman <koenholman@gmail.com>
2019-03-14 13:39:50 +0100

    begin activities show page

--- resources/views/account/show.blade.php
+++ resources/views/account/show.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container m-0 p-0 vh-100 justify-content-center ">
+    <div class="row m-0 p-0 vh-50 vertical-align bg-info ">
+        <div class="col-12 mt-4 ml-4 p-0 h1 "><span>{{ $user->name}}</div>
+        <div class="col-12 mt-4 ml-4 p-0"><span>photo: </span>{{ $user->photo}}</div>
+        <div class="col-12 mt-4 ml-4 p-0 h5"><span>skills:</span>{{ $user->skills}}</div>
+    </div>
+    <div class="row mt-4 p-0 vh-50">
+        <div class="col-12 mt-4 ml-4 p-0 h4"><span>About: </span> {{$user->about_me}}</div>
+        <div class="col-12 m-0 p-0 ml-4 h6 text-secondary"><span>email: </span>{{ $user->email}}</div>
+        <div class="col-12 m-0 p-0 ml-4 sh6 text-secondary"><span>phone: </span>{{ $user->phone}}</div>
+        <div class="col-12 m-4 p-0 h4"><span>achievement: </span> {{$user->achievement}}</div>
+        <div class="col-12 m-4 p-0 h4"><span>review: </span> {{$user->review}}</div>
 
-<div class="container m-0 p-0 fluid justify-content-center ">
-        <div class="row m-0 p-0 vertical-align">
-                <div class="col-12 m-4 h1"><span>{{ $user->name}}</div>
-                <div class="col-12 m-4"><span>photo: </span>{{ $user->photo}}</div>
-                <div class="col-12 m-4 h5"><span>skills:</span>{{ $user->skills}}</div>
-        </div>
-        <div class="row mt-4 p-0">
-                <div class="col-12 m-4 h4"><span>About: </span> {{$user->about_me}}</div>
-                <div class="col-12 m-4 h4"><span>email: </span>{{ $user->email}}</div>
-                <div class="col-12 m-4 h4"><span>phone: </span>{{ $user->phone}}</div>
-        </div>
-        <a class="btn btn-danger m-4" href="/account/{{ $user->id }}/edit">Edit account</a>
+    </div>
+    <a class="btn btn-danger m-4" href="/account/{{ $user->id }}/edit">Edit account</a>
 </div>
 @endsection
 
