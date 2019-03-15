dabfa0973ba495c951402041f9c02e5db8fc39c9
KCTHolman <koenholman@gmail.com>
2019-03-15 08:24:46 +0100

    basic layout chat index.blade

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="my-container container m-0 p-0 vh-100 justify-content-center ">
+<div class="my-container container m-0 p-0 vh-100 justify-content-center">
     <div class="my-row row m-0 p-0 vh-50 vertical-align bg-warning ">
-        Chat works
+        <h2>Chat</h2>
     </div>
     <div class="my-row row mt-4 p-0 vh-50">
 </div>
