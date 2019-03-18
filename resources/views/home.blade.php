8f7bfa68ea19bdc97ff76af851c150c35f5bf095
Bob Smekens <bobsmekens90@gmail.com>
2019-03-18 11:24:28 +0100

    added picutres

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="card">
-  <img class="card-img-top" src="{{ URL::asset ('img/one.jpg') }}" alt="Card image cap">
+  <img class="card-img-top" src="{{ URL::asset ('img/activity1.jpg') }}" alt="Card image cap">
   <div class="card-body"><h2 class="card-title">MEETUP</h2>
     <p class="text-wrap">Meetup or skill up, we have what you want!</p>
     <p>Welcome {{ $user[0]->name }}</p>
