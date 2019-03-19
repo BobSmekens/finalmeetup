e10d6fc7221a96e9fbd8b87d7decda8ce9d0ae37
KCTHolman <koenholman@gmail.com>
2019-03-19 11:06:08 +0100

    belongstomany working, not implemented in views

--- app/Activity.php
+++ app/Activity.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
    public function users(){
-       return $this->belongsToMany(users::class);
+       return $this->belongsToMany('App\User');
    }
 }
