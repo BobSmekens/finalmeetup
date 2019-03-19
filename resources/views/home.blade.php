e3de7a22b3bdd05b03883dd305b728dae0030b92
vagrant <vagrant@homestead>
2019-03-19 14:54:24 +0000

    blueprint design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 
-<div class="home-container">
+<!-- <div class="home-container"> 
   <div class="top">
-    <img class="top-image" src="{{ URL::asset ('img/activity1.jpg') }}" alt="Card image cap">
-  </div>
+    <img class="top-image" src="{{ URL::asset ('.png') }}" alt="Card image cap">
+  </div> -->
   
-  <div class="bottom"><h2 class="">MEETUP</h2>
-    <p class="">Meetup or skill up, we have what you want!</p>
+  <div class="bottom"><h1 class=""></h1>
+    <p class="">A PLATFORM TO ENGAGE IN ACTIVITIES OR TO LEARN NEW SKILLS</p>
     <p>Welcome {{ $user[0]->name }}</p>
   </div>
 
