87db71efba9386c2a0c7f7dff15d06f536967619
KCTHolman <koenholman@gmail.com>
2019-03-20 14:37:37 +0100

    added user feedback if activity is full

--- resources/views/activities/show.blade.php
+++ resources/views/activities/show.blade.php
@@ -7,13 +7,19 @@
     </div>
     <div class="my-row row m-4 p-4 justify-content-center">
         <div class="col-12">Maximum amount of people: {{ $activity->max_persons}}</div>
-        <div class="col-12"> {{ $activity->description}}</div>
-        <div class="col-12"> {{ $activity->category}}</div>
+        <div class="col-12">{{ $activity->description}}</div>
+        <div class="col-12">{{ $activity->category}}</div>
 
     </div>
-    
-    <a class="bob-btn-visable" href="/activities/{{ $activity->id }}/meetup">schrijf je in voor activiteit</a>
-    <a class="bob-btn-visable" href="/activities/{{ $activity->id }}/edit">pas activiteit aan</a>
+    @if ($activity->users->count()>=$activity->max_persons)
+            <span class="alert">activiteit is vol
+            </span>
+            @else
+            <a class="bob-btn-visable" href="/activities/{{ $activity->id }}/meetup">schrijf je in voor activiteit</a>
+
+            @endif</div>
+
+            <a class="bob-btn-visable" href="/activities/{{ $activity->id }}/edit">pas activiteit aan</a>
 
 
     <form action="/activities/{{ $activity->id }}" method="POST">
