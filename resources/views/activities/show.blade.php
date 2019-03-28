dfbbd78d7d75946e4004a50a41dada222a7df296
Bob Smekens <bobsmekens90@gmail.com>
2019-03-28 15:38:31 +0100

    leider niet uitschrijven

--- resources/views/activities/show.blade.php
+++ resources/views/activities/show.blade.php
@@ -5,21 +5,32 @@
     <div class="activities-signup-link">
         @if ($activity->users->count()>=$activity->max_persons && $signedInUser == true)
             <span class="">Je doet mee en de activiteit zit vol</span>
-            <form class="subscribe-activities-button-container" action="/calendar/{{ $activity->id}}/unsub" method="POST">
-                {{ method_field('DELETE') }}
-                {{ csrf_field() }}
-                <button class="activity-subscribe-btn" type="submit">Uitschrijven</button>
-            </form>
+            @if(Auth::user()->id != $activity->posted_by)
+                <form class="subscribe-activities-button-container" action="/calendar/{{ $activity->id}}/unsub" method="POST">
+                    {{ method_field('DELETE') }}
+                    {{ csrf_field() }}
+                
+                    <button class="activity-subscribe-btn" type="submit">Uitschrijven</button>
+                </form>
+            @else
+                    <h3>Je bent de organisator</h3>
+            @endif
         @elseif ($activity->users->count()>=$activity->max_persons && $signedInUser == false)
             <span class="">Geen plekken</span>
         @elseif ($signedInUser == true)
-            <form class="subscribe-activities-button-container" action="/calendar/{{ $activity->id}}/unsub" method="POST">
-                {{ method_field('DELETE') }}
-                {{ csrf_field() }}
-                <button class="activity-subscribe-btn" type="submit">Uitschrijven</button>
-            </form>
+            @if(Auth::user()->id != $activity->posted_by)
+                <form class="subscribe-activities-button-container" action="/calendar/{{ $activity->id}}/unsub" method="POST">
+                    {{ method_field('DELETE') }}
+                    {{ csrf_field() }}
+                    <button class="activity-subscribe-btn" type="submit">Uitschrijven</button>
+                </form>
+            @else
+                <h3>Je bent de organisator</h3>
+            @endif
         @else
-            <a class="bob-btn-visable" href="/activities/{{ $activity->id }}/meetup">Inschrijven</a>
+            @if(Auth::user()->id != $activity->posted_by)
+                <a class="bob-btn-visable" href="/activities/{{ $activity->id }}/meetup">Inschrijven</a>
+            @endif
         @endif
     </div>
 </div>
